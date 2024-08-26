0d7d179f03f33f271b0ff1900e868c97c0f4090a
Gowrishankar <96763224+GowrishankarWeb@users.noreply.github.com>
2024-08-26 23:26:48 +0530

    Minor changes in seller.java

--- src/main/java/com/sellers/entity/Seller.java
+++ src/main/java/com/sellers/entity/Seller.java
@@ -1,12 +1,17 @@
 package com.sellers.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
 @Data
 public class Seller {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private int rating;
